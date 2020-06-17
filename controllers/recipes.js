b6e6b138b4533f1e6d684898cb7dbba437ba0c09
Alexandre Santos <37197689+Alexsantosjr@users.noreply.github.com>
2020-06-17 00:25:39 -0300

    Added system admin

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 const data = require("../data.json")
-const newreceitas = require("../data")
+const newreceitas = require("../data.js")
 
 //Padrao
 exports.receitas = function(req, res){
